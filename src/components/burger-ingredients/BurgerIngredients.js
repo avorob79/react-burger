f65f877e9e8feddcee07aad1b2f72fa7cc42734f
avorobyev <avorobyev@npkinform.ru>
2023-01-19 16:30:26 +0300

    Скорректирована разметка раздела BurgerIngredients, настроена работа табов для отображения информации

--- src/components/burger-ingredients/BurgerIngredients.js
+++ src/components/burger-ingredients/BurgerIngredients.js
@@ -1,46 +1,68 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredient from '../burger-ingredient/BurgerIngredient';
 import IngredientDetails from '../ingredient-details/IngredientDetails';
 import Modal from '../modal/Modal';
 import styles from './BurgerIngredients.module.css';
-import {ingredientType} from '../../utils/types';
+import { ingredientType } from '../../utils/types';
 
-function BurgerIngredients(props) {
-  const [current, setCurrent] = React.useState('one');
+function BurgerIngredients({ data }) {
+  const [currentTab, setCurrentTab] = React.useState("bun");
   const [details, setDetails] = React.useState(null);
-  const showDetails = (item) => setDetails(item);
+
+  const onTabClick = (tab) => {
+    setCurrentTab(tab);
+    const element = document.getElementById(tab);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  }
+
+  const buns = useMemo(() => data.filter((item) => item.type === "bun"),
+    [data]
+  );
+
+  const sauces = useMemo(() => data.filter((item) => item.type === "sauce"),
+    [data]
+  );
+
+  const mains = useMemo(() => data.filter((item) => item.type === "main"),
+    [data]
+  );
+
   const hideDetails = (e) => {
     setDetails(null);
     if (!!e && !!e.stopPropagation) {
       e.stopPropagation();
     }
   };
+
   return (
     <section className={styles.burgerIngredients}>
       <h1 className="text text_type_main-large pt-10 pb-5">Соберите бургер</h1>
       <div className={styles.tabs}>
-        <Tab value="one" active={current === 'one'} onClick={setCurrent}>Булки</Tab>
-        <Tab value="two" active={current === 'two'} onClick={setCurrent}>Соусы</Tab>
-        <Tab value="three" active={current === 'three'} onClick={setCurrent}>Начинки</Tab>
+        <Tab value="bun" active={currentTab === "bun"} onClick={onTabClick}>Булки</Tab>
+        <Tab value="sauce" active={currentTab === "sauce"} onClick={onTabClick}>Соусы</Tab>
+        <Tab value="main" active={currentTab === "main"} onClick={onTabClick}>Начинки</Tab>
       </div>
-      <div className={styles.ingredients}>
-        <p className={`text text_type_main-medium ${styles.type} mt-10`}>Булки</p>
-        {props.data.filter(item => item.type === "bun").map((item, index)=>(
-          <BurgerIngredient key={item._id} counter={1} item={item} showDetails={showDetails} extraClass="ml-4 mr-2 mb-2 mt-6" />
-          )
-        )}
-        <p className={`text text_type_main-medium ${styles.type} mt-8`}>Соусы</p>
-        {props.data.filter(item => item.type === "sauce").map((item, index)=>(
-          <BurgerIngredient key={item._id} counter={1} item={item} showDetails={showDetails} extraClass="ml-4 mr-2 mb-2 mt-6" />
-          )
-        )}
-        <p className={`text text_type_main-medium ${styles.type} mt-8`}>Начинки</p>
-        {props.data.filter(item => item.type === "main").map((item, index)=>(
-          <BurgerIngredient key={item._id} counter={1} item={item} showDetails={showDetails} extraClass="ml-4 mr-2 mb-2 mt-6" />
-          )
-        )}
+      <div className={styles.allIngredients}>
+        <h3 id="bun" className={`text text_type_main-medium mt-10 mb-6`}>Булки</h3>
+        <div className={`${styles.ingredients} pl-4`}>
+          {buns.map((item) => (
+            <BurgerIngredient key={item._id} counter={1} item={item} showDetails={setDetails} />
+          ))}
+        </div>
+        <h3 id="sauce" className={`text text_type_main-medium mt-10 mb-6`}>Соусы</h3>
+        <div className={`${styles.ingredients} pl-4`}>
+          {sauces.map((item) => (
+            <BurgerIngredient key={item._id} counter={1} item={item} showDetails={setDetails} />
+          ))}
+        </div>
+        <h3 id="main" className={`text text_type_main-medium mt-10 mb-6`}>Начинки</h3>
+        <div className={`${styles.ingredients} pl-4`}>
+          {mains.map((item) => (
+            <BurgerIngredient key={item._id} counter={1} item={item} showDetails={setDetails} />
+          ))}
+        </div>
       </div>
       {!!details &&
         <Modal title="Детали ингредиента" onClose={hideDetails}>
@@ -52,7 +74,7 @@ function BurgerIngredients(props) {
 }
 
 BurgerIngredients.propTypes = {
-  data: PropTypes.arrayOf(ingredientType).isRequired
+  data: PropTypes.arrayOf(ingredientType.isRequired).isRequired
 };
 
 export default BurgerIngredients;
